2dfac3effdaa0d0e2d9d9b9cc82b5ac9a79b8bc0
jwenndt <39074783+jwenndt@users.noreply.github.com>
2018-06-15 09:55:48 -0500

    Codecademy generated from the project submission page.

---
+++ Codecademy/code.sql
@@ -0,0 +1,133 @@
+--1
+--SELECT *
+--FROM subscriptions
+--LIMIT 100; 
+ --2
+--SELECT MAX(subscription_start) AS 'Earliest Subscription'
+--   ,MIN(subscription_start) AS 'Latest Subscription'
+--FROM subscriptions;
+--3
+WITH months AS
+	(SELECT
+  	'2017-01-01' AS first_day,
+  	'2017-01-31' AS last_day
+  UNION 
+   SELECT 
+   	'2017-02-01' AS first_day,
+   	'2017-02-28' AS last_day
+   UNION 
+   	SELECT 
+   	'2017-03-01' AS first_day,
+   	'2017-03-31' AS last_day),
+--4
+Cross_Join AS 
+ 	(SELECT *
+  FROM subscriptions
+  CROSS JOIN months),
+--5 & 6
+status AS (
+  SELECT
+    id, 
+    first_day AS month, 
+    CASE
+  		WHEN (segment = 87)
+       AND (subscription_start < first_day)
+      AND (subscription_end > first_day OR subscription_end IS NULL)
+        THEN 1
+      ELSE 0
+    END AS is_active_87, 
+    CASE
+     WHEN (segment = 30)
+      AND (subscription_start < first_day)
+      AND (subscription_end > first_day OR subscription_end IS NULL)
+        THEN 1
+      ELSE 0
+    END AS is_active_30,
+  	CASE
+  		WHEN (segment = 87)
+       AND (subscription_end BETWEEN first_day AND last_day)
+  		THEN 1
+  	ELSE 0
+  END AS is_canceled_87,
+ 		CASE
+    	WHEN (segment = 30)
+      AND (subscription_end BETWEEN first_day AND last_day)
+      THEN 1
+    ELSE 0
+  END AS is_canceled_30
+FROM Cross_Join),
+--7
+status_aggregate AS (
+  SELECT 
+    month,
+    SUM(is_active_87) AS sum_active_87, 
+    SUM(is_active_30) AS sum_active_30, 
+    SUM(is_canceled_87) AS sum_canceled_87,
+    SUM(is_canceled_30) AS sum_canceled_30 
+  FROM status
+  GROUP BY month) 
+--8
+SELECT month,
+	1.0 * sum_canceled_87 / sum_active_87 AS churn_rate_87, 	1.0 * sum_canceled_30 / sum_active_30 AS churn_rate_30
+FROM status_aggregate
+--LIMIT 100
+
+;
+ 
+----------------------------------------------------------
+
+WITH months AS
+	(SELECT
+  	'2017-01-01' AS first_day,
+  	'2017-01-31' AS last_day
+  UNION 
+   SELECT 
+   	'2017-02-01' AS first_day,
+   	'2017-02-28' AS last_day
+   UNION 
+   	SELECT 
+   	'2017-03-01' AS first_day,
+   	'2017-03-31' AS last_day),
+--4
+Cross_Join AS 
+ 	(SELECT *
+  FROM subscriptions
+  CROSS JOIN months),
+--5 & 6
+status AS (
+  SELECT
+    id, 
+    segment,
+    first_day AS month, 
+    CASE
+     WHEN (subscription_start < first_day)
+      AND (subscription_end > first_day OR subscription_end IS NULL)
+        THEN 1
+      ELSE 0
+    END AS is_active,
+ 		CASE
+    	WHEN (subscription_end BETWEEN first_day AND last_day)
+      THEN 1
+    ELSE 0
+  END AS is_canceled
+FROM Cross_Join),
+--7
+status_aggregate AS (
+  SELECT 
+    month,
+  	segment,
+    SUM(is_active) AS sum_active, 
+    SUM(is_canceled) AS sum_canceled 
+  FROM status
+  GROUP BY month,
+	segment) 
+--8
+SELECT month,
+	segment,
+	1.0 * sum_canceled / sum_active AS churn_rate
+FROM status_aggregate
+GROUP BY month,
+	segment
+--LIMIT 100
+
+;
